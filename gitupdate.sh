018efbc670009f05cdf1bd1c50304d65f2bf97b3
tylerterzigni <tylerterzigni@gmail.com>
2019-12-31 09:03:34 -0800

    Test

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
 cd /home/homeassistant/.homeassistant
-source /srv/homeassistant/bin/activate
+
 git add .
 git status
 echo -n "Enter the Description for the Change: " [Minor Update]
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
+
 exit
